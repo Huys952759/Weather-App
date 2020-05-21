b979ce74da2971f076424127c2782dc98e87e993
Yaosong Hu <yaosonghu@Yaosongs-MacBook-Pro.local>
2020-05-21 16:13:24 +0800

    Updates

---
+++ precache-manifest.4832971f1ae9294af44b8b970d23fac3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2abfe8efebf0acfcd1ed9cb4f618d8",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "b65ba43972bcdbd7c283",
+    "url": "/Weather-App/static/css/2.877a7df7.chunk.css"
+  },
+  {
+    "revision": "40e31b8a20742ef79f46",
+    "url": "/Weather-App/static/css/main.f992185d.chunk.css"
+  },
+  {
+    "revision": "b65ba43972bcdbd7c283",
+    "url": "/Weather-App/static/js/2.7032a2d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Weather-App/static/js/2.7032a2d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40e31b8a20742ef79f46",
+    "url": "/Weather-App/static/js/main.1c8fada6.chunk.js"
+  },
+  {
+    "revision": "786e689b55e703e556c0",
+    "url": "/Weather-App/static/js/runtime-main.d7b74fb0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather-App/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather-App/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather-App/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather-App/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather-App/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
